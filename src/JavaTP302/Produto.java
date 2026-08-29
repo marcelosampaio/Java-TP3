a83bf11cc6b31fd11aca4222316d641547e5be07
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-08-28 23:58:44 -0300

    feat: Exercicio 2

---
+++ src/JavaTP302/Produto.java
@@ -0,0 +1,10 @@
+package JavaTP302;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
+    //  O atributo nome garante a identificação precisa do item no catálogo e no caixa.
+    //  O preco viabiliza o cálculo financeiro das vendas e a definição de margens de lucro.
+    //  A quantidadeEmEstoque permite o controle do inventário.
+}
