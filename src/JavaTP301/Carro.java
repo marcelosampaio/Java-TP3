7241ab5f5871e0e108b8ddbd140f0829bce14159
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-08-28 23:47:25 -0300

    feat: Exercicio 1

---
+++ src/JavaTP301/Carro.java
@@ -0,0 +1,13 @@
+package JavaTP301;
+
+public class Carro {
+    String marca;
+    String modelo;
+    int ano;
+
+    void exibirInformacoes() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+}
---
+++ src/JavaTP301/JavaTP301.java
@@ -0,0 +1,13 @@
+package JavaTP301;
+
+public class JavaTP301 {
+
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.marca = "Honda";
+        carro.modelo = "Civic";
+        carro.ano = 2026;
+
+        carro.exibirInformacoes();
+    }
+}
