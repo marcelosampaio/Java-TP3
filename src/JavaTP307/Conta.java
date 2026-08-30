25063c32ed8c6d2f10a74a622e031e68abf87ad3
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-08-30 20:53:06 -0300

    feat: Exercicio 7

---
+++ src/JavaTP307/Conta.java
@@ -0,0 +1,9 @@
+package JavaTP307;
+
+public class Conta {
+    private String titular;
+    private int numero;
+    private String agencia;
+    private double saldo;
+    private String dataAbertura;
+}
