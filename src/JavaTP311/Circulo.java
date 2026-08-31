8d00f2d420ee94abd59451a57a827169b9e0b46d
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-08-31 16:43:44 -0300

    feat: Exercicio 11

---
+++ src/JavaTP311/Circulo.java
@@ -0,0 +1,10 @@
+package JavaTP311;
+
+public class Circulo {
+    private double raio;
+
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+}
+
---
+++ src/JavaTP311/Esfera.java
@@ -0,0 +1,9 @@
+package JavaTP311;
+
+public class Esfera {
+    private double raio;
+
+    public double calcularVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
+    }
+}
