b11b2c06e35bf627f4227c72a20f787efebd7210
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2026-08-31 15:47:35 -0300

    feat: Exercicio 10

---
+++ src/JavaTP310/Circulo.java
@@ -0,0 +1,7 @@
+package JavaTP310;
+
+public class Circulo {
+    private double raio;
+    // o raio é fundamental porque determina o tamanho do círculo e pode
+    // ser utilizado cálculo de área.
+}
---
+++ src/JavaTP310/Esfera.java
@@ -0,0 +1,7 @@
+package JavaTP310;
+
+public class Esfera {
+    private double raio;
+    // o raio é fundamental porque determina o tamanho da esfera e poderá
+    // ser utilizado para cálculo de volume.
+}
